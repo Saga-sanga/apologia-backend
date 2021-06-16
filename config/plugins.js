c08c6b3976e7ef5178228e7c39784df2e7435068
Saga-sanga <vulcan248@gmail.com>
2021-06-16 22:25:13 +0530

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    provider: 'sendgrid',
+    providerOptions: {
+      apiKey: env('SENDGRID_API_KEY'),
+    },
+    settings: {
+      defaultFrom: 'recksonk94@gmail.com',
+      defaultReplyTo: 'recksonk94@gmail.com',
+    },
+  },
+  // ...
+});
