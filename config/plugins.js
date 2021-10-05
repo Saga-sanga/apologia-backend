c196ba6ec498bac726ae6ddae7a0a4b07358bb11
Saga-sanga <vulcan248@gmail.com>
2021-10-05 14:59:48 +0530

    Updated strapi to latest version and added a testimony Content type

--- config/plugins.js
+++ config/plugins.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
       apiKey: env('SENDGRID_API_KEY'),
     },
     settings: {
-      defaultFrom: 'recksonk94@gmail.com',
-      defaultReplyTo: 'recksonk94@gmail.com',
+      defaultFrom: 'mizoapologetics@gmail.com',
+      defaultReplyTo: 'mizoapologetics@gmail.com',
     },
   },
   // ...
