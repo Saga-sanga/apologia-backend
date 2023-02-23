a1e425da5c35f9b81676b88ff5598e53db89b4e4
Saga-sanga <vulcan248@gmail.com>
2023-02-23 11:58:36 +0530

    Feat: Add new collections

---
+++ src/api/bible-study/controllers/bible-study.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * bible-study controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::bible-study.bible-study');
